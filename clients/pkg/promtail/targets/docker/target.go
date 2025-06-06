80fef473b5d6b2eaaf12dd94b5de4b26b3316fd5
Joe Harvey <51208233+jharvey10@users.noreply.github.com>
2025-06-06 14:01:58 -0400

    fix: Remove unused docker_types import for Docker API v27+ compatibility

--- clients/pkg/promtail/targets/docker/target.go
+++ clients/pkg/promtail/targets/docker/target.go
@@ -10,7 +10,6 @@ import (
 	"sync"
 	"time"
 
-	docker_types "github.com/docker/docker/api/types"
         "github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 	"github.com/docker/docker/pkg/stdcopy"
